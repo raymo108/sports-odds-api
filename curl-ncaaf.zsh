f155886bfbda3f4c4ed7377d4d69de707bee8a11
Shane Raymond <60206015+raymo108@users.noreply.github.com>
2024-08-29 22:55:39 -0400

    Curl odds-api to gather NFL and NCAAF betting lines

---
+++ curl-ncaaf.zsh
@@ -0,0 +1,1 @@
+curl -X GET "https://api.the-odds-api.com/v4/sports/aamericanfootball_ncaaf/odds/?apiKey=YOUR_API_KEY&regions=us&markets=h2h,spreads,totals&oddsFormat=american&dateFormat=iso" -o nfl_odds.json
